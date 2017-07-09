2a50a9906e5d3758db1979931f4044b7d7baa332
Florian Scherer <f.scherer@buildingradar.com>
2017-07-09 16:49:07 +0200

    wrote MPU file

--- linkinhart/linkinhart.ino
+++ linkinhart/linkinhart.ino
@@ -1,47 +1,43 @@
 #include "NoteButton.h"
-#include<Wire.h>
+#include "MPU.h"
 
-const int MPU_addr=0x68;
-double AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; //These will be the raw data from the MPU6050.
-uint32_t timer; //it's a timer, saved as a big-ass unsigned int.  We use it to save times from the "micros()" command and subtract the present time in microseconds from the time stored in timer to calculate the time for each loop.
-double compAngleX, compAngleY; //These are the angles in the complementary filter
-#define degconvert 57.2957786 //there are like 57 degrees in a radian.
+// define the chords
+int c_dur[] = {60, 64, 67, 72};
+int g_dur[] = {67, 71, 74, 79};
+int a_moll[] = {69, 72, 76, 81};
+int f_dur[] = {65, 69, 72, 77};
+int d_moll[] = {62, 65, 69, 74};
+int drums[] = {36, 42, 40, 39};
 
-int result;
-float val;
+int *tonart = c_dur;
 
-int velocity = 127;
-
-// Keys
-int noteA = 60;
-int noteB = 64;
-int noteC = 67;
-int noteD = 72;
-
-// Drums
-//int noteA = 36;
-//int noteB = 42;
-//int noteC = 40;
-//int noteD = 39;  
+// four notes of one triad
+int root_note;
+int third_note;
+int fifth_note;
+int root_note_high;
 
 int inputA = 5;
 int inputB = 6;
 int inputC = 7;
 int inputD = 8;
 
-int inputE = 8;
-int inputF = 9;
-int inputG = 10;
-int inputH = 11;
-
-int potRead = 0;
-int potState = 0;
-int potInput = A0;
+int inputE = 9;
+int inputF = 10;
+int inputG = 11;
+int inputH = 12;
 
 NoteButton buttonA (inputA);
 NoteButton buttonB (inputB);
 NoteButton buttonC (inputC);
 NoteButton buttonD (inputD);
+
+const int MPU_addr=0x68;
+MPU mpu_object (MPU_addr);
+
+double compAngleY;
+int result;
+int velocity = 127;
 
 void setup() {
   // put your setup code here, to run once:
@@ -57,130 +53,40 @@ void setup() {
   pinMode(inputG, INPUT);
   pinMode(inputH, INPUT);
 
-  // Set up MPU 6050:
-  Wire.begin();
-  #if ARDUINO >= 157
-  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
-  #else
-    TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
-  #endif
-
-  
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
-  delay(100);
-
-  //setup starting angle
-  //1) collect the data
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-  //2) calculate pitch and roll
-  double roll = atan2(AcY, AcZ)*degconvert;
-  double pitch = atan2(-AcX, AcZ)*degconvert;
-
-  //3) set the starting angle to this pitch and roll
-  double gyroXangle = roll;
-  double gyroYangle = pitch;
-  double compAngleX = roll;
-  double compAngleY = pitch;
-
-  //start a timer
-  timer = micros();
+  mpu_object.setup();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  if (digitalRead(inputE) == LOW) {
-    noteA = 67;
-    noteB = 71;
-    noteC = 74;
-    noteD = 79;
-     
-  }
-  else if (digitalRead(inputF) == LOW) {
-    noteA = 69;
-    noteB = 72;
-    noteC = 76;
-    noteD = 81; 
-  }
-  else if (digitalRead(inputG) == LOW) {
-    noteA = 65;
-    noteB = 69;
-    noteC = 72;
-    noteD = 77;
-  }
-  else if (digitalRead(inputH) == LOW) {
-    noteA = 62;
-    noteB = 65;
-    noteC = 69;
-    noteD = 74; 
-  }
-  else {
-    noteA = 60;
-    noteB = 64;
-    noteC = 67;
-    noteD = 72; 
-  }
+  // set the chord via the inputs E-H
+  if (digitalRead(inputE) == LOW)
+    tonart = g_dur;
+  else if (digitalRead(inputF) == LOW)
+    tonart = a_moll;
+  else if (digitalRead(inputG) == LOW)
+    tonart = f_dur;
+  else if (digitalRead(inputH) == LOW)
+    tonart = d_moll;
+  else
+    tonart = c_dur;
+
+  root_note = tonart[0];
+  third_note = tonart[1];
+  fifth_note = tonart[2];
+  root_note_high = tonart[3];
   
-  buttonA.processButton(noteA, velocity);
-  buttonB.processButton(noteB, velocity);
-  buttonC.processButton(noteC, velocity);
-  buttonD.processButton(noteD, velocity);
+  buttonA.processButton(root_note, velocity);
+  buttonB.processButton(third_note, velocity);
+  buttonC.processButton(fifth_note, velocity);
+  buttonD.processButton(root_note_high, velocity);
   
   delay(10);
 
-  //Now begins the main loop. 
-  //Collect raw data from the sensor.
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  
-  double dt = (double)(micros() - timer) / 1000000; //This line does three things: 1) stops the timer, 2)converts the timer's output to seconds from microseconds, 3)casts the value as a double saved to "dt".
-  timer = micros(); //start the timer again so that we can calculate the next dt.
+  mpu_object.process();
+  compAngleY = mpu_object.get_comp_angle_y();
 
-  //the next two lines calculate the orientation of the accelerometer relative to the earth and convert the output of atan2 from radians to degrees
-  //We will use this data to correct any cumulative errors in the orientation that the gyroscope develops.
-  double roll = atan2(AcY, AcZ)*degconvert;
-  double pitch = atan2(-AcX, AcZ)*degconvert;
-
-  //The gyroscope outputs angular velocities.  To convert these velocities from the raw data to deg/second, divide by 131.  
-  //Notice, we're dividing by a double "131.0" instead of the int 131.
-  double gyroXrate = GyX/131.0;
-  double gyroYrate = GyY/131.0;
-
-  //THE COMPLEMENTARY FILTER
-  //This filter calculates the angle based MOSTLY on integrating the angular velocity to an angular displacement.
-  //dt, recall, is the time between gathering data from the MPU6050.  We'll pretend that the 
-  //angular velocity has remained constant over the time dt, and multiply angular velocity by 
-  //time to get displacement.
-  //The filter then adds a small correcting factor from the accelerometer ("roll" or "pitch"), so the gyroscope knows which way is down. 
-  compAngleX = 0.99 * (compAngleX + gyroXrate * dt) + 0.01 * roll; // Calculate the angle using a Complimentary filter
-  compAngleY = 0.99 * (compAngleY + gyroYrate * dt) + 0.01 * pitch; 
-
-  val = (127. / 360.) * compAngleY + 63.5;
-  result = val;
+  result = (127. / 360.) * compAngleY + 63.5;
   sendMidi(176, 95, result);
 
 }
